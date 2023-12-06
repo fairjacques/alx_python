1973d7262366d681e7232e13b25960a7f1270334
fairjacques <fairjacques5@gmail.com>
2023-12-06 14:56:03 +0200

    me

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,7 +1,7 @@
-word= "school"
+word= "holberton"
 word_first_3= word[0:3]
 word_last_2= word[-2:]
 middle_word= word[1:-1]
-print("word_first_3:{}".format(word_first_3))
-print("word_last_2:{}".format(word_last_2))
-print("middle_word:{}".format(middle_word))
+print("word first 3:{}".format(word_first_3))
+print("word last 2:{}".format(word_last_2))
+print("middle word:{}".format(middle_word))
