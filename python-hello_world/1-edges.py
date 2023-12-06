9a864e7699c56411e8c6bad411e562b2699caaba
fairjacques <fairjacques5@gmail.com>
2023-12-06 15:50:58 +0200

    y

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,8 +1,8 @@
 word= "Holberton"
-updateword= word.replace ("Holberton" , "School")
-word_first_3= word[0:3]
-word_last_2= word[-2:]
-middle_word= word[1:-1]
+updateword= "School"
+word_first_3= updateword[0:3]
+word_last_2= updateword[-2:]
+middle_word= updateword[1:-1]
 print("First 3 letters: {}".format(word_first_3))
 print("Last 2 letters: {}".format(word_last_2))
 print("Middle word: {}".format(middle_word))
