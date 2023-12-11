7e611894e6fdd8eb377a9b7ae2112adb2040ad9f
fairjacques <fairjacques5@gmail.com>
2023-12-11 19:22:00 +0200

    hhhhh

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -4,12 +4,10 @@ if number >= 5:
     last_digit= number % 10
 else :
     last_digit= -(-number % 10)        
-
 if last_digit >5:
     out_put ="and is greater than 5"
 elif last_digit ==0:
     out_put="and is 0"
 else :
     out_put="and is less than 6 not 0"    
-
-print ("last_digit",number,"is",last_digit)        
+print ("last_digit of",number,"is", last_digit,out_put)        
