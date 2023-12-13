44ca575371ac893ec2343ef363d3669572dfa7ce
fairjacques <fairjacques5@gmail.com>
2023-12-13 17:20:35 +0200

    hhhhh

---
+++ python-hello_world/6-print_comb3.py
@@ -0,0 +1,5 @@
+for u in range(100):
+    if u <99:
+        print("{:02}".format(u),end=", ")
+    else:
+        print("{:03}".format(u))
---
+++ python-hello_world/edge.py
@@ -0,0 +1,9 @@
+for a in range (10):
+    for b in range(a+1,10):
+      if a*10+b <89:
+        print("{:02}".format(a*10+b),end=", ")
+      else:
+        print("{:0}".format(a*10+b))
+     
+       
+    
