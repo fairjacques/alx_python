7a7a3a0d41ccf4faee5da54404db3f759dad02d6
fairjacques <fairjacques5@gmail.com>
2024-02-21 15:56:17 +0200

    yes

--- python-functions/2-temperature.py
+++ python-functions/2-temperature.py
@@ -1,7 +1,8 @@
 def convert_to_celsius(fahrenheit):
     return (fahrenheit - 32) * 5/9
 
-print(convert_to_celsius(100))
-print(convert_to_celsius(-40))
-print(convert_to_celsius(-459.67))
-print(convert_to_celsius(32))
+print:convert_to_celsius(100)
+print:convert_to_celsius(-40)
+print:convert_to_celsius(-459.67)
+print:convert_to_celsius(32)
+print:convert_to_celsius(98.6)
